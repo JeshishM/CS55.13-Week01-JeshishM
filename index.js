844d5093b332544ecd1e974672e73db0e68dd817
Jeshish Maharjan <89277664+JeshishM@users.noreply.github.com>
2021-08-28 06:09:23 +0000

    Changed code

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
+// Node Server
 
+const http = require('http');
+const server = http.createServer(
+
+	function(req, res){
+		console.log(req.url );
+
+		let mydata;
+    if ( req.url === "/hello" ) {
+      mydata = "Hey there. - Welcome to my Server...";
+    } else {
+      mydata = "I don't know you!";
+    }
+    mydata = mydata + " " + "Thank You!";
+
+		res.writeHead( 200, { "Content-Type": "text/plain" } );
+		res.end(mydata);
+	}
+);
+server.listen(8080, "0.0.0.0");
+console.log("server has started");
 
 
